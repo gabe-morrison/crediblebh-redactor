32ac91968ae7aee109ed700c7be179d08ce77ad1
gabe-morrison <88749996+gabe-morrison@users.noreply.github.com>
2023-01-05 16:23:43 -0600

    Create script.js

---
+++ script.js
@@ -0,0 +1,30 @@
+const clientNames = [
+  ['',''],
+  ['',''],
+  ['',''],
+  ['',''],
+  ['',''],
+  ['',''],
+  ['',''],
+  ['',''],
+  ['',''],
+  ['',''],
+]
+
+const redactAll = () => {
+  clientNames.forEach((client)=>{
+    redact(client[0],client[1]);
+  })
+}
+
+const redact = (firstName,lastName) => {
+  const list = document.querySelectorAll('a, td, .Answer');
+  const regex = new RegExp(`${firstName}|${lastName}`);
+  list.forEach((el)=>{
+    el.childNodes.forEach((node)=>{    
+      if (node.nodeType === 3 && node.data.includes(firstName || lastName)) {
+        node.data = node.data.replaceAll(regex,'[redacted]');
+      }    
+    })
+  })
+}
