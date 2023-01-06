17dc8d678e5fe2c6dea98cbd84587fc8239a8940
gabe-morrison <88749996+gabe-morrison@users.noreply.github.com>
2023-01-06 12:54:08 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,5 @@
 const clientNames = [
-  ['',''],
-  ['',''],
-  ['',''],
-  ['',''],
-  ['',''],
-  ['',''],
-  ['',''],
-  ['',''],
-  ['',''],
-  ['',''],
+  ['','']
 ]
 
 const redactAll = () => {
@@ -19,12 +10,17 @@ const redactAll = () => {
 
 const redact = (firstName,lastName) => {
   const list = document.querySelectorAll('a, td, .Answer');
-  const regex = new RegExp(`${firstName}(?!h)|${lastName}`);
+  const regex = new RegExp(`(?<![A-Za-z])${firstName}(?![A-Za-z])|(?<![A-Za-z])${lastName}(?![A-Za-z])`);
+  const insuranceRegex = new RegExp("UCARE|MHCP|UBH|HP\s[0-9]|HP :");
   list.forEach((el)=>{
     el.childNodes.forEach((node)=>{    
       if (node.nodeType === 3 && node.data.includes(firstName || lastName)) {
         node.data = node.data.replaceAll(regex,'[redacted]');
-      }    
+      } else if (node.nodeType === 3 && insuranceRegex.test(node.data)) {        
+        node.data = "[redacted]";
+      }
     })
   })
 }
+
+redactAll()
